1af19f77f5dddb23d6b81f6b1b774b7f7269cfcf
alenzer <alenzer0902@gmail.com>
2022-11-29 03:21:33 -0500

    update config

--- config.example.js
+++ config.example.js
@@ -8,111 +8,109 @@ module.exports = {
   //// ACCOUNT INFO ////
   // The account seed phrase to use for deployment
   mnemonic:
-    'enlist hip relief stomach skate base shallow young switch frequent cry park',
+    '',
   // Your STARS address
-  account: 'stars1...',
+  account: '',
 
   //// API CONFIG ////
-  // The RPC endpoint for Stargaze testnet
+  // The RPC endpoint for Stargaze, Elgafar Testnet
   rpcEndpoint: 'https://rpc.elgafar-1.stargaze-apis.com/',
-  // The RPC endpoint for Stargaze mainnet
+  // The RPC endpoint for Stargaze, mainnet
   // rpcEndpoint: 'https://rpc.stargaze-apis.com/',
   // NFT.storage endpoint
   nftStorageEndpoint: 'https://api.nft.storage',
   // NFT.storage API key
-  nftStorageApiKey: '',
+  nftStorageApiKey:
+    '',
   // Pinata API Key (optional)
   pinataApiKey: '',
   // Pinata Secret Key (optional)
-  pinataSecretKey: '',
+  pinataSecretKey:
+    '',
 
   //// COLLECTION INFO ////
   // The name of your collection
-  name: 'Collection Name',
+  name: 'AquaNauts',
   // The 3-7 letter ticker symbol for your collection
-  symbol: 'SYM',
+  symbol: 'AQUA',
   // Project description
-  description: 'An awesome NFT series',
+  description:
+    'NFTs packed with value and utility that make a real difference in the world',
   // Link to image to use as the main image for the collection.
   // Either IPFS or valid http links allowed. Gif compatible.
   // (at least 500 x 500 pixels)
   image:
-    'ipfs://bafybeigi3bwpvyvsmnbj46ra4hyffcxdeaj6ntfk5jpic5mx27x6ih2qvq/images/1.png',
+    'https://bafybeigclyfgaysx73ch43hvqfdx6jt26saocpf4gggd3dyblh3pixtlda.ipfs.nftstorage.link/',
   // External_link is optional. Gif compatible
   //   external_link:
   // 'https://c.tenor.com/o656qFKDzeUAAAAC/rick-astley-never-gonna-give-you-up.gif',
-  // The address for royalties to go to (may be the same as `account`)
-  // Comment out both below if not using royalties
-  // royaltyPaymentAddress: 'stars1...',
+  // The address for royalites to go to (may be the same as `account`)
+  // Comment out both below if not using royalites
+  royaltyPaymentAddress: 'stars1uyjwsywch5h6x9pwrakj46y6fcr3a9w66mkn90',
   // Royalty share: 1 = 100%, 0.1 = 10%
-  // royaltyShare: '0.1',
+  royaltyShare: '0.1',
 
   //// WHITELIST CONTRACT (OPTIONAL) ////
   // A list of whitelisted addresses that will be able to purchase the sale early
   // Comment out if not using a whilelist
-  // whitelist: ['stars1..', 'stars1...'],
+  whitelist: ['stars12v06zrrhw0vs83t83svsddgl4ndfmk9cny2wu3'],
   // The date when the whitelist only purchasing period ends and everyone can buy (in ISO format)
-  // whitelistStartTime: '2022-03-11T21:00:00.000Z',
-  // whitelistEndTime: '2022-03-13T21:00:00.000Z',
+  whitelistStartTime: '2022-11-30T18:00:00.000Z',
+  whitelistEndTime: '2022-11-31T06:00:00.000Z',
   // The price (in STARS) for the whitelist (minimum 25 STARS)
-  // whitelistPrice: 50,
+  whitelistPrice: 4300,
   // The Per Address Limit during whitelist period this can be different than the main public limit
-  // whitelistPerAddressLimit: 5,
+  whitelistPerAddressLimit: 5,
   // The number of members in the whitelist (max 5000, each 1000 is 100 STARS)
-  // whitelistMemberLimit: 100,
+  whitelistMemberLimit: 300,
   // The contract address for your whitelist contract
   // Get this after running `yarn whitelist`
-  // whitelistContract: 'stars1...',
+  whitelistContract:
+    'stars1854vdrkhhd8wuuss3j37skk90ue9a0rt9vqm9r6tp62uj69l77vsvwpwhq',
 
   //// MINTER CONTRACT ////
-  // The base URI to be used to programmatically mint tokens
-  baseTokenUri: 'ipfs://...',
+  // The base URI to be used to programatically mint tokens
+  baseTokenUri:
+    'ipfs://bafybeiaq3soaeiele2zx3n2qomksia352pooa5wbk5scnczzvucaqbad7m/galaxy4phyYu',
   // The number of tokens to mint
-  numTokens: 100,
+  numTokens: 1000,
   // The price (in STARS) for your NFTs (minimum 50 STARS)
-  mintPrice: 100,
+  mintPrice: 5400,
   // The max amount of NFTs an address can mint
-  perAddressLimit: 1,
+  perAddressLimit: 10,
   // The date when the sale goes live
   // If whitelist is enabled, only whitelisted addresses will be able to purchase
   // startTime in ISO format
-  startTime: '2022-08-11T19:00:00.000Z',
-  // The date when secondary sales goes live
-  // If this is not enabled a default offset will be used (usually 2 weeks)
-  // That is set by governance
-  // startTradingTime: '2022-08-11T19:00:00.000Z',
+  startTime: '2022-11-30T06:00:00.000Z',
   // The minter contract address
   // Get this after running `yarn minter`
-  minter: 'stars1...',
+  minter: 'stars1wpjzt0kdw2rkmca282e6a2nz5jdwukcrt6p9myaa75g37cev930qf2lapc',
   // SG721 contract address
   // Get this after running `yarn minter`
-  sg721: 'stars1...',
+  sg721: 'stars1kmlfx4gs2k72kck7yg54udw2l550j0penfsjhp535kpk7ta8ndqqhzf7z3',
 
-  //// CONTRACT CODE IDs: Testnet ////
+  //// CONTRACT CODE IDs: Double Double Testnet ////
   // The code ID for sg721_base
   sg721BaseCodeId: 274,
   // The code ID for vending_minter
-  vendingMinterCodeId: 275,
+  vendingMinterCodeId: 573,
+  // The code ID for vending_factory contract
+  vendingFactoryCodeId: 572,
   // Addr for vending factory contract
   vendingFactory:
-    'stars1j4qn9krchp5xs8nued4j4vcr4j654wxkhf7acy76734xe5fsz08sku28s2',
+    'stars1ue6ld376p9tzkj4qg4x6s0qwtkf0rapqfsknmhtvre0autxkhzmq4crj55',
   // The code ID for the whitelist contract
   whitelistCodeId: 277,
-  // The marketplace contract address
-  //   marketplace:
-  //     'stars18cszlvm6pze0x9sz32qnjq4vtd45xehqs8dq7cwy8yhq35wfnn3qgzs5gu',
+  // marketplace:
+  //   'stars18cszlvm6pze0x9sz32qnjq4vtd45xehqs8dq7cwy8yhq35wfnn3qgzs5gu',
 
   // //// CONTRACT CODE IDs: Mainnet ////
   // // The code ID for sg721
-  // sg721BaseCodeId: 15,
+  // sg721CodeId: 1,
   // // The code ID for the minter contract
-  // vendingMinterCodeId: 16,
-  // // Addr for vending factory contract
-  // vendingFactory:
-  //  'stars15p5xmrj4ryfh39pdvlphdr64kunm4zrvq3p6rku82zlahkn63vtqm3gmpt',
+  // minterCodeId: 2,
   // // The code ID for the whitelist contract
-  // whitelistCodeId: 18,
-  // The marketplace contract address
+  // whitelistCodeId: 3,
   //   marketplace:
   //       'stars1fvhcnyddukcqfnt7nlwv3thm5we22lyxyxylr9h77cvgkcn43xfsvgv0pl',
 };
